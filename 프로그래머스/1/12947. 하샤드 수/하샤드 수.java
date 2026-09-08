a5b334064779936f99b775fe8c5615cbc78ad9e5
dayou-dev <jodogfoot@gmail.com>
2026-09-08 23:24:40 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean solution(int x) {
+        int sum = 0;
+        int origin = x;
+        while (x > 0){
+            sum += x % 10;
+            x /= 10;
+        }
+        return origin % sum == 0;
+    }
+}
